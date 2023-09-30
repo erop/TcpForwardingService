f51125a0ff65fdde198e0f0ccf7a90c552292c77
Egor Ushakov <eushakov@gmail.com>
2023-09-30 21:16:47 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using TcpForwardingService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
