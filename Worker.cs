d872c19397da3ac269bc76e8abe3d52779ba6cd2
Egor Ushakov <eushakov@gmail.com>
2023-09-30 21:52:35 +0400

    Add options to worker

--- Worker.cs
+++ Worker.cs
@@ -1,12 +1,19 @@
+using Microsoft.Extensions.Options;
+using TcpForwardingService.Configuration;
+
 namespace TcpForwardingService;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly SourceSettings _sourceSettings;
+    private readonly DestinationsSettings _destinationsSettings;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, IOptions<SourceSettings> sourceSettings, IOptions<DestinationsSettings> destinationsSettings)
     {
         _logger = logger;
+        _sourceSettings = sourceSettings.Value;
+        _destinationsSettings = destinationsSettings.Value;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
