a4df161eb74623357d7a1b14417bd870ca61e8fa
Egor Ushakov <eushakov@gmail.com>
2023-10-01 00:30:46 +0400

    Handle listener socket error

--- Worker.cs
+++ Worker.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System.Net.Sockets;
 using Microsoft.Extensions.Options;
 using TcpForwardingService.Configuration;
 
@@ -5,11 +7,12 @@ namespace TcpForwardingService;
 
 public class Worker : BackgroundService
 {
+    private readonly DestinationsSettings _destinationsSettings;
     private readonly ILogger<Worker> _logger;
     private readonly SourceSettings _sourceSettings;
-    private readonly DestinationsSettings _destinationsSettings;
 
-    public Worker(ILogger<Worker> logger, IOptions<SourceSettings> sourceSettings, IOptions<DestinationsSettings> destinationsSettings)
+    public Worker(ILogger<Worker> logger, IOptions<SourceSettings> sourceSettings,
+        IOptions<DestinationsSettings> destinationsSettings)
     {
         _logger = logger;
         _sourceSettings = sourceSettings.Value;
@@ -18,10 +21,31 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
-        }
+        while (true)
+            try
+            {
+                var endpoint = new IPEndPoint(IPAddress.Parse(_sourceSettings.LocalIp), _sourceSettings.Port);
+                var listener = new TcpListener(endpoint);
+                listener.Start();
+
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                    await Task.Delay(1000, stoppingToken);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            catch (SocketException e)
+            {
+                _logger.LogError("Socket exception: {Message}", e.Message);
+                await Task.Delay(1000, stoppingToken);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Unexpected error occurred: {Message}", e.Message);
+                await Task.Delay(1000, stoppingToken);
+            }
     }
 }
